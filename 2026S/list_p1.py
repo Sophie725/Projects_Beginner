9bf26a844d5f94ff9eec3602a259bc7b489d470a
Wonbin Bae <109917320+Sophie725@users.noreply.github.com>
2026-04-21 08:50:08 -0700

    Update list_p1.py
    Finish by April 21st. Use indexing to make functions.

--- 2026S/list_p1.py
+++ 2026S/list_p1.py
@@ -5,6 +5,8 @@ def drop_first(l: list) -> list:
     # This function should drop the first element of the list l 
     # and return the modified list.
     # Fill in the code here
+    if l:
+        l.pop(0)
 
     return l
 
@@ -12,6 +14,8 @@ def drop_last(l: list) -> list:
     # This function should drop the last element of the list l
     # and return the modified list.
     # Fill in the code here
+    if l:
+        l.pop(-1)
 
     return l
 
@@ -19,6 +23,7 @@ def drop_odd_indices(l: list) -> list:
     # This function should drop all the elements at odd indices from the list l
     # and return the modified list.
     # Fill in the code here
+    l = l[::2]
 
     return l
 
@@ -26,6 +31,7 @@ def drop_even_indices(l: list) -> list:
     # This function should drop all the elements at even indices from the list l
     # and return the modified list.
     # Fill in the code here
+    l = l[1::2]
 
     return l
 
@@ -33,6 +39,8 @@ def drop_value(v: int, l: list) -> list:
     # Fill in the code here
     # Your code should be able to handle errors, 
     # such as v not being in l or l being empty
+    if v in l:
+        l.remove(v)
 
     return l
 
@@ -40,12 +48,19 @@ def drop_odd_values(l: list) -> list:
     # This function should drop all the odd numbers from the list l
     # and return the modified list.
     # Fill in the code here
+    even_only = []
+    for i in range(len(l) - 1, -1, -1):
+        if l[i] % 2 == 1:
+            l.pop(i)
 
     return l
 
 def drop_even_values(l: list) -> list:
     # This function should drop all the even numbers from the list l
     # Fill in the code here
+    for i in range(len(l) - 1, -1, -1):
+        if l[i] % 2 == 0:
+            l.pop(i)
 
     return l
 
@@ -53,13 +68,16 @@ def drop_at(index: int, l: list) -> list:
     # Fill in the code here
     # Your code should be able to handle errors, 
     # such as n being out of range or l being empty
-
+    if index >= 0 and index < len(l):
+        del l[index]
     return l
 
 def drop_until(index: int, l: list) -> list:
     # Fill in the code here
     # Your code should be able to handle errors, 
     # such as n being out of range or l being empty
+    if index >= 0 and index < len(l):
+        del l[0:index]
 
     return l
 
@@ -67,6 +85,9 @@ def drop_from(index: int, l: list) -> list:
     # Fill in the code here
     # Your code should be able to handle errors, 
     # such as n being out of range or l being empty
+    if index >= 0 and index < len(l):
+        last_index = len(l)
+        del l[index: last_index]
 
     return l
 
@@ -74,9 +95,10 @@ def drop_range(start: int, end: int, l: list) -> list:
     # Fill in the code here
     # Your code should be able to handle errors, 
     # such as n being out of range or l being empty
+    if start >= 0 and end <= len(l) and start < end:
+        del l[start:end]
 
     return l
-
 
 import random
 
